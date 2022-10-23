61edb6708445593f5dc21caa965da53832bb6ec4
zenghanmu <2231593226@qq.com>
2022-10-23 13:56:42 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-essential',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['vue', '@typescript-eslint'],
+    rules: {
+        'prettier/prettier': [
+            0,
+            {
+                semi: true,
+                parser: 'flow',
+            },
+        ],
+        '@typescript-eslint/ban-types': 0,
+        '@typescript-eslint/no-explicit-any': 0,
+    },
+};
